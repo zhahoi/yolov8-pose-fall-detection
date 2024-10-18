cdfbd9bcb77f29da0471838d3fbe4879dccae8de
HongyingZhang <104057195+zhahoi@users.noreply.github.com>
2024-10-18 08:59:50 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "yolov8_pose.h"
 
-#define YOLOV8_PARAM "/home/hit/Project/yolov8-pose-fall-detection/weights/yolov8-pose-human-opt.param"
-#define YOLOV8_BIN "/home/hit/Project/yolov8-pose-fall-detection/weights/yolov8-pose-human-opt.bin"
+#define YOLOV8_PARAM "/home/hit/Project/yolov8-pose-human/weights/yolov8-pose-human-opt.param"
+#define YOLOV8_BIN "/home/hit/Project/yolov8-pose-human/weights/yolov8-pose-human-opt.bin"
+#define SAVE_PATH "/home/hit/Project/yolov8-pose-human/outputs"
 
 std::unique_ptr<Yolov8Pose> yolov8Pose(new Yolov8Pose(YOLOV8_PARAM, YOLOV8_BIN, false));
 
@@ -26,6 +27,8 @@ int main(int argc, char** argv)
         yolov8Pose->detect_yolov8(image, objects);
         cv::Mat result;
         yolov8Pose->detect_objects(image, result, objects, SKELETON, KPS_COLORS, LIMB_COLORS);
+
+        cv::imwrite(SAVE_PATH + std::string("/output.jpg"), result);
         cv::imshow("Detection Result", result);
         cv::waitKey(0);
 
@@ -36,6 +39,14 @@ int main(int argc, char** argv)
             return -1;
         }
 
+        // 获取视频的基本信息
+        int frame_width = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
+        int frame_height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+        int fps = static_cast<int>(cap.get(cv::CAP_PROP_FPS));
+
+        // 定义视频编码器和输出文件名
+        cv::VideoWriter outputVideo(SAVE_PATH + std::string("/output.mp4"), cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, cv::Size(frame_width, frame_height));
+
         cv::Mat frame;
         while (cap.read(frame)) {
             std::vector<Object> objects;
@@ -43,6 +54,9 @@ int main(int argc, char** argv)
             cv::Mat result;
             yolov8Pose->detect_objects(frame, result, objects, SKELETON, KPS_COLORS, LIMB_COLORS);
             yolov8Pose->draw_fps(result);
+
+            // 写入视频
+            outputVideo.write(result);
             cv::imshow("Detection Result", result);
 
             // 按 'q' 键退出
@@ -50,6 +64,11 @@ int main(int argc, char** argv)
                 break;
             }
         }
+
+        // 释放资源
+        cap.release();
+        outputVideo.release();
+        cv::destroyAllWindows();
 
     } else {
         std::cerr << "Invalid input type. Please use 'image' or 'video'." << std::endl;
